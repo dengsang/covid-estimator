4580745d2d0fdf38328d30d1386c2ab64d31e3c9
dengsang <dengsangt@gmail.com>
2020-04-15 01:32:54 +0300

    Estimation of covid19 data

--- src/estimator.py
+++ src/estimator.py
@@ -1,42 +1,77 @@
-import itertools
 import json
-
-from src import impact, severeimpact
+import math
 
 
 def estimator(data):
-    data = data
-    impacts = impact.Impact.impact(data)
-    severe_impacts = severeimpact.Severe.severe_impact(data)
+    string_data = json.dumps(data)
+    data_object = json.loads(string_data)
 
-    result = {'data': data, 'impact': impacts, 'severe_impact': severe_impacts}
+    reported_cases = data_object.get("reportedCases")
+    period_type = data_object.get("periodType")
+    time_to_elapse = data_object.get("timeToElapse")
+    total_hospital_beds = data_object.get("totalHospitalBeds")
+    population = data_object.get("population")
+
+    estimate_currently_infected = reported_cases * 10
+
+    if period_type == "months":
+        elapse = math.trunc(time_to_elapse / 3) * 30
+    elif period_type == "weeks":
+        elapse = math.trunc(time_to_elapse / 3) * 7
+    elif period_type == "days":
+        elapse = math.trunc(time_to_elapse / 3)
+    else:
+        elapse = 0
+
+    estimate_projected_infections = estimate_currently_infected * (2 ** elapse)
+    estimate_cases_for_icu = math.trunc(0.5 * estimate_projected_infections)
+    estimate_cases_for_ventilators = math.trunc(0.2 * estimate_projected_infections)
+
+    estimate_severe_cases = math.trunc(0.15 * estimate_projected_infections)
+    severe_hospital_beds = math.trunc(0.35 * total_hospital_beds)
+    estimate_bed_space_availability = severe_hospital_beds - estimate_severe_cases
+    estimate_severe_cases = math.trunc(0.15 * estimate_projected_infections)
+
+    average_daily_dollar = math.trunc(estimate_projected_infections * 0.65 * 1.5) / 30
+    estimate_dollars_in_flight = average_daily_dollar * population
+
+    impact_data = {'currently_infected': estimate_currently_infected,
+                   'projected_infections': estimate_projected_infections,
+                   'estimate_cases_for_icu': estimate_cases_for_icu,
+                   'estimate_cases_for_ventilators': estimate_cases_for_ventilators,
+                   'estimate_dollars_in_flight': estimate_dollars_in_flight}
+
+    severe_data = {'currently_infected': estimate_currently_infected,
+                   'projected_infections': estimate_projected_infections,
+                   'estimate_severe_cases': estimate_severe_cases,
+                   'bed_space_availability': estimate_bed_space_availability,
+                   'estimate_cases_for_icu': estimate_cases_for_icu,
+                   'estimate_cases_for_ventilators': estimate_cases_for_ventilators,
+                   'estimate_dollars_in_flight': estimate_dollars_in_flight}
+
+    result = {'data': data, 'impact': impact_data, 'severe_impact': severe_data}
 
     print(result)
 
-    return result
+    return json.dumps(result)
 
 
-def covid19_impact_estimator(data):
-    if __name__ == '__main__':
-        estimate_currently_infected = impact.Impact.impact(data).estimate_currently_infected
-        estimate_projected_infections = impact.Impact.impact(data).estimate_projected_infections
-        estimate_severe_cases = severeimpact.Severe.severe_impact(data).estimate_severe_cases
-        estimate_bed_space_availability = severeimpact.Severe.severe_impact(data).estimate_bed_space_availability
-        estimate_cases_for_icu = severeimpact.Severe.severe_impact(data).estimate_cases_for_icu
-        estimate_cases_for_ventilators = severeimpact.Severe.severe_impact(data).estimate_cases_for_ventilators
-        estimate_dollars_in_flight = severeimpact.Severe.severe_impact(data).estimate_dollars_in_flight
+def covid19_impact_estimator():
+    input_data = {
+        "region": {
+            "name": "Africa",
+            "avgAge": 19.7,
+            "avgDailyIncomeInUSD": 5,
+            "avgDailyIncomePopulation": 0.71
+        },
+        "periodType": "days",
+        "timeToElapse": 58,
+        "reportedCases": 674,
+        "population": 66622705,
+        "totalHospitalBeds": 1380614
+    }
+    estimator(input_data)
 
-        # def estimator():
-        estimate = itertools.chain(data,
-                                   # challenge1
-                                   estimate_currently_infected, estimate_projected_infections,
-                                   # challenge2
-                                   estimate_severe_cases, estimate_bed_space_availability,
-                                   # challenge3
-                                   estimate_cases_for_icu, estimate_cases_for_ventilators, estimate_dollars_in_flight
-                                   )
 
-        for estimates in estimate:
-            return json.dumps(estimates.__dict__)
-
-        return estimator(data)
+if __name__ == "__main__":
+    covid19_impact_estimator()
